cb2e8ddd8b14f08e6d2e670ee42e941212cd7197
SAIKRISHNA-01 <58844486+SAIKRISHNA-01@users.noreply.github.com>
2023-09-20 17:28:58 +0530

    Create python_project.py

---
+++ python_project.py
@@ -0,0 +1,8 @@
+#Python Project for Data Engineering
+
+Objectives
+
+Run the ETL process
+Extract bank and market cap data from the JSON file bank_market_cap.json
+Transform the market cap currency using the exchange rate data
+Load the transformed data into a seperate CSV
